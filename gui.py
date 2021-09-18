d1465d0f88359949eacde775d6ed09878f9ad611
C-ffeeStain <deaconavery2006@gmail.com>
2021-09-18 17:24:54 -0500

    modify gitignore some more

--- gui.py
+++ gui.py
@@ -55,6 +55,7 @@ class GUI(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
+        
 
     def initUI(self):
         self.setWindowTitle("Random Person Generator")
